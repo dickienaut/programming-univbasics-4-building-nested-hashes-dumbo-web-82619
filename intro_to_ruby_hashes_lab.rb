83545d4fc4fb788d3c1047666bbc5aec02a5a940
lyle dickie <lyle.e.dickie@gmail.com>
2019-08-14 04:07:41 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,7 +22,8 @@ def monopoly_with_third_tier
 
   hsh = {:railroads => {}}
   hsh[:railroads][:pieces] = 4
-  hsh[:railroads][:rent_in_dollars] = {:names => nil, :key2 => nil, :key3 => nil, :key4 => nil}
+  hsh[:railroads][:rent_in_dollars] = {:key1 => nil, :key2 => nil, :key3 => nil, :key4 => nil}
+  hsh[:railroads][:names] = {}
   hsh
 end
 
